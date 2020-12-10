b817e3dfba036c6fdc3e0882f8b2787ac92110da
felipeserna <feserna86@gmail.com>
2020-12-10 10:46:39 -0500

    Task 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Calculates the derivative of a polynomial
+"""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial"""
+    if type(poly) is not list:
+        return None
+
+    deri = []
+    for coef in poly:
+        deri.append(coef * poly.index(coef))
+    if deri.index(0) == 0:
+        deri.pop(0)
+    return deri
