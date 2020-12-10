59b28fabab02ca56458f4a18a883e3b11b724057
felipeserna <feserna86@gmail.com>
2020-12-10 17:45:13 -0500

    Task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Calculates the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    """Calculates the integral of a polynomial"""
+    if type(poly) is not list:
+        return None
+
+    """if C is not int:
+        return None"""
+
+    if len(poly) == 0:
+        return None
+
+    inte = [C]
+    for coef in range(len(poly)):
+        inte.append(poly[coef]/(coef + 1))
+    return inte
