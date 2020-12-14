5950d6150e17e04e5c8e58ad03b97dde9d71f462
felipeserna <feserna86@gmail.com>
2020-12-14 14:22:10 -0500

    Task 4

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -4,6 +4,9 @@ Represents an exponential distribution
 """
 
 
+e = 2.7182818285
+
+
 class Exponential:
     """Represents an exponential distribution"""
     def __init__(self, data=None, lambtha=1.):
@@ -26,3 +29,12 @@ class Exponential:
             else:
                 # calculate lambtha of the data
                 self.lambtha = float(1/(sum(data)/len(data)))
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given time period
+        x is the time period
+        """
+        if x <= 0:
+            return 0
+        return (self.lambtha * e**((-1)*self.lambtha*x))
