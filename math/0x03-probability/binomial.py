978feeeee96330c7c815c2c8398c258a1e24dfa1
felipeserna <feserna86@gmail.com>
2020-12-15 11:17:12 -0500

    Task 11

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -38,3 +38,22 @@ class Binomial:
                 self.n = round(mean/p)
                 # Recalculating p for more precision because of rounding n
                 self.p = float(mean/self.n)
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        k is the number of “successes”
+        """
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        return (self.factorial(self.n)*self.p**k*(1-self.p)**(self.n - k) /
+                (self.factorial(self.n - k)*self.factorial(k)))
+
+    def factorial(self, k):
+        """Calculates the factorial of k"""
+        if k == 0:
+            return 1
+        else:
+            return (k * self.factorial(k-1))
