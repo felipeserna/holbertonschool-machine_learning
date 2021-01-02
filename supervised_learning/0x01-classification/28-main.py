ed23f2e159df2ed1472fbf272dbb140c859381b7
felipeserna <feserna86@gmail.com>
2021-01-02 12:24:42 -0500

    new 28-main from Guillaume

--- supervised_learning/0x01-classification/28-main.py
+++ supervised_learning/0x01-classification/28-main.py
@@ -2,10 +2,9 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pickle
 
-Deep3 = __import__('27-deep_neural_network').DeepNeuralNetwork
-Deep4 = __import__('28-deep_neural_network').DeepNeuralNetwork
+Deep27 = __import__('27-deep_neural_network').DeepNeuralNetwork
+Deep28 = __import__('28-deep_neural_network').DeepNeuralNetwork
 one_hot_encode = __import__('24-one_hot_encode').one_hot_encode
 one_hot_decode = __import__('25-one_hot_decode').one_hot_decode
 
@@ -24,58 +23,58 @@ Y_valid_one_hot = one_hot_encode(Y_valid, 10)
 Y_test_one_hot = one_hot_encode(Y_test, 10)
 
 print('Sigmoid activation:')
-deep3 = Deep3.load('27-output.pkl')
-A_one_hot3, cost3 = deep3.evaluate(X_train, Y_train_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_train == A3) / Y_train.shape[0] * 100
-print("Train cost:", cost3)
-print("Train accuracy: {}%".format(accuracy3))
-A_one_hot3, cost3 = deep3.evaluate(X_valid, Y_valid_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_valid == A3) / Y_valid.shape[0] * 100
-print("Validation cost:", cost3)
-print("Validation accuracy: {}%".format(accuracy3))
-A_one_hot3, cost3 = deep3.evaluate(X_test, Y_test_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_test == A3) / Y_test.shape[0] * 100
-print("Test cost:", cost3)
-print("Test accuracy: {}%".format(accuracy3))
+deep27 = Deep27.load('27-output.pkl')
+A_one_hot27, cost27 = deep27.evaluate(X_train, Y_train_one_hot)
+A27 = one_hot_decode(A_one_hot27)
+accuracy27 = np.sum(Y_train == A27) / Y_train.shape[0] * 100
+print("Train cost:", cost27)
+print("Train accuracy: {}%".format(accuracy27))
+A_one_hot27, cost27 = deep27.evaluate(X_valid, Y_valid_one_hot)
+A27 = one_hot_decode(A_one_hot27)
+accuracy27 = np.sum(Y_valid == A27) / Y_valid.shape[0] * 100
+print("Validation cost:", cost27)
+print("Validation accuracy: {}%".format(accuracy27))
+A_one_hot27, cost27 = deep27.evaluate(X_test, Y_test_one_hot)
+A27 = one_hot_decode(A_one_hot27)
+accuracy27 = np.sum(Y_test == A27) / Y_test.shape[0] * 100
+print("Test cost:", cost27)
+print("Test accuracy: {}%".format(accuracy27))
 
 fig = plt.figure(figsize=(10, 10))
 for i in range(100):
     fig.add_subplot(10, 10, i + 1)
     plt.imshow(X_test_3D[i])
-    plt.title(A3[i])
+    plt.title(A27[i])
     plt.axis('off')
 plt.tight_layout()
 plt.show()
 
 print('\nTanh activaiton:')
 
-deep4 = Deep4.load('4-saved.pkl')
-A_one_hot4, cost4 = deep4.train(X_train, Y_train_one_hot, iterations=100,
+deep28 = Deep28.load('28-saved.pkl')
+A_one_hot28, cost28 = deep28.train(X_train, Y_train_one_hot, iterations=100,
                                 step=10, graph=False)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_train == A4) / Y_train.shape[0] * 100
-print("Train cost:", cost4)
-print("Train accuracy: {}%".format(accuracy4))
-A_one_hot4, cost4 = deep4.evaluate(X_valid, Y_valid_one_hot)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_valid == A4) / Y_valid.shape[0] * 100
-print("Validation cost:", cost4)
-print("Validation accuracy: {}%".format(accuracy4))
-A_one_hot4, cost4 = deep4.evaluate(X_test, Y_test_one_hot)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_test == A4) / Y_test.shape[0] * 100
-print("Test cost:", cost4)
-print("Test accuracy: {}%".format(accuracy4))
-deep4.save('4-output')
+A28 = one_hot_decode(A_one_hot28)
+accuracy28 = np.sum(Y_train == A28) / Y_train.shape[0] * 100
+print("Train cost:", cost28)
+print("Train accuracy: {}%".format(accuracy28))
+A_one_hot28, cost28 = deep28.evaluate(X_valid, Y_valid_one_hot)
+A28 = one_hot_decode(A_one_hot28)
+accuracy28 = np.sum(Y_valid == A28) / Y_valid.shape[0] * 100
+print("Validation cost:", cost28)
+print("Validation accuracy: {}%".format(accuracy28))
+A_one_hot28, cost28 = deep28.evaluate(X_test, Y_test_one_hot)
+A28 = one_hot_decode(A_one_hot28)
+accuracy28 = np.sum(Y_test == A28) / Y_test.shape[0] * 100
+print("Test cost:", cost28)
+print("Test accuracy: {}%".format(accuracy28))
+deep28.save('28-output')
 
 fig = plt.figure(figsize=(10, 10))
 for i in range(100):
     fig.add_subplot(10, 10, i + 1)
     plt.imshow(X_test_3D[i])
-    plt.title(A4[i])
+    plt.title(A28[i])
     plt.axis('off')
 plt.tight_layout()
 plt.show()
