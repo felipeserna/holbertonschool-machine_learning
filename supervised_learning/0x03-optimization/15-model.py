74bcac4e3822b621503b5017cb3e7a0b1da49a8b
felipeserna <feserna86@gmail.com>
2021-01-14 17:43:23 -0500

    put it all together 5

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -52,45 +52,38 @@ def create_layer(prev, n, activation):
 
 def create_batch_norm_layer(prev, n, activation):
     """
-    creates a batch normalization layer for a neural network in tensorflow
-    :param prev: activated output of the previous layer
-    :param n: number of nodes in the layer to be created
-    :param activation: activation function that should be used on
-    the output of the layer
-    :return:  tensor of the activated output for the layer
+    - prev is the activated output of the previous layer
+    - n is the number of nodes in the layer to be created
+    - activation is the activation function that should be used
+      on the output of the layer
+    - you should use the tf.layers.Dense layer as the base layer with
+      kernal initializer
+      tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    - your layer should incorporate two trainable parameters, gamma and beta,
+      initialized as vectors of 1 and 0 respectively
+    - you should use an epsilon of 1e-8
+    - Returns: a tensor of the activated output for the layer
     """
-    if activation is None:
-        A = create_layer(prev, n, activation)
-        return A
+    # He et al. initialization for the layer weights
+    kernal_init = tf.contrib.layers.\
+        variance_scaling_initializer(mode="FAN_AVG")
 
-    # implement He et. al initialization for the layer weights
-    initializer = \
-        tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-
-    # dense layer model
-    model = tf.layers.Dense(units=n,
-                            kernel_initializer=initializer)
-    Z = model(prev)
-
-    # incorporation of trainable parameters beta and gamma
-    # for scale and offset
-    gamma = tf.Variable(tf.constant(1.0, shape=[n]),
-                        name='gamma', trainable=True)
-    beta = tf.Variable(tf.constant(0.0, shape=[n]),
-                       name='beta', trainable=True)
-    epsilon = tf.constant(1e-8)
-
-    # normalization parameter calculation
-    mean, variance = tf.nn.moments(Z, axes=0)
-
-    # Normalization over result after activation (with mean and variance)
-    # and later adjusting with beta and gamma for
-    # offset and scale respectively
-    adjusted = tf.nn.batch_normalization(x=Z, mean=mean, variance=variance,
-                                         offset=beta, scale=gamma,
-                                         variance_epsilon=epsilon)
-
-    A = activation(adjusted)
+    base_layer = tf.layers.Dense(units=n, kernel_initializer=kernal_init,
+                                 name='base_layer')
+    X = base_layer(prev)
+    # Calculate the mean and variance of X
+    mu, var = tf.nn.moments(X, axes=[0])
+    gamma = tf.Variable(tf.constant(1.0, shape=(1, n)), trainable=True,
+                        name='gamma')
+    beta = tf.Variable(tf.constant(0.0, shape=(1, n)), trainable=True,
+                       name='beta')
+    # returns the normalized, scaled, offset tensor
+    Z = tf.nn.batch_normalization(x=X, mean=mu, variance=var,
+                                  offset=beta, scale=gamma,
+                                  variance_epsilon=1e-8,
+                                  name='Z')
+    # activation function
+    A = activation(Z)
     return A
 
 
