2ea5333e1f94af3f41ad826dff61b6bdd345b5c7
felipeserna <feserna86@gmail.com>
2021-01-14 18:03:54 -0500

    put it all together

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -159,47 +159,32 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     return learning_rate
 
 
-def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
-          beta2=0.999, epsilon=1e-8, decay_rate=1, batch_size=32,
-          epochs=5, save_path='/tmp/model.ckpt'):
+def model(Data_train, Data_valid, layers, activations, alpha=0.001,
+          beta1=0.9, beta2=0.999, epsilon=1e-8, decay_rate=1,
+          batch_size=32, epochs=5, save_path='/tmp/model.ckpt'):
     """
-    Data_train is a tuple containing the training inputs and
-               training labels, respectively
-    Data_valid is a tuple containing the validation inputs and
-               validation labels, respectively
-    layers is a list containing the number of nodes in each
-               layer of the network
-    activation is a list containing the activation functions
-               used for each layer of the network
-    alpha is the learning rate
-    beta1 is the weight for the first moment of Adam Optimization
-    beta2 is the weight for the second moment of Adam Optimization
-    epsilon is a small number used to avoid division by zero
-    decay_rate is the decay rate for inverse time decay of
-               the learning rate (the corresponding decay step should be 1)
-    batch_size is the number of data points that should be in a mini-batch
-    epochs is the number of times the training should pass
-               through the whole dataset
-    save_path is the path where the model should be saved to
-    Returns: the path where the model was saved
+    Builds, trains, and saves a neural network model in tensorflow using
+    Adam optimization,
+    mini-batch gradient descent,
+    learning rate decay,
+    and batch normalization
     """
-    # getting data_batch
-    steps = Data_train[0].shape[0] / batch_size
-    if (steps).is_integer() is True:
-        steps = int(steps)
-    else:
-        steps = int(steps) + 1
-
     X_train = Data_train[0]
     Y_train = Data_train[1]
     X_valid = Data_valid[0]
     Y_valid = Data_valid[1]
 
-    x = tf.placeholder(tf.float32, shape=[None, Data_train[0].shape[1]],
+    steps = X_train.shape[0] / batch_size
+    if (steps).is_integer() is True:
+        steps = int(steps)
+    else:
+        steps = int(steps) + 1
+
+    x = tf.placeholder(tf.float32, shape=[None, X_train.shape[1]],
                        name='x')
     tf.add_to_collection('x', x)
 
-    y = tf.placeholder(tf.float32, shape=[None, Data_train[1].shape[1]],
+    y = tf.placeholder(tf.float32, shape=[None, Y_train.shape[1]],
                        name='y')
     tf.add_to_collection('y', y)
 
@@ -239,22 +224,21 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
                 [loss, accuracy],
                 feed_dict={x: X_valid, y: Y_valid})
 
-            # where {epoch} is the current epoch
             print("After {} epochs:".format(epoch))
 
-            # where {train_cost} is the cost of the model
+            # train_cost is the cost of the model
             # on the entire training set
             print("\tTraining Cost: {}".format(train_cost))
 
-            # where {train_accuracy} is the accuracy of the model
+            # train_accuracy is the accuracy of the model
             # on the entire training set
             print("\tTraining Accuracy: {}".format(train_accuracy))
 
-            # where {valid_cost} is the cost of the model
+            # valid_cost is the cost of the model
             # on the entire validation set
             print("\tValidation Cost: {}".format(valid_cost))
 
-            # where {valid_accuracy} is the accuracy of the model
+            # valid_accuracy is the accuracy of the model
             # on the entire validation set
             print("\tValidation Accuracy: {}".format(valid_accuracy))
 
@@ -267,37 +251,37 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
                 # shuffle data, both training set and labels
                 X_shuffled, Y_shuffled = shuffle_data(X_train, Y_train)
 
-                # iteration within epoch
+                # mini-batch within epoch
                 for step_number in range(steps):
 
                     # data selection mini batch from training set and labels
                     start = step_number * batch_size
 
                     end = (step_number + 1) * batch_size
-                    if end > Data_train[0].shape[0]:
-                        end = Data_train[0].shape[0]
+                    if end > X_train.shape[0]:
+                        end = X_train.shape[0]
 
                     X = X_shuffled[start:end]
                     Y = Y_shuffled[start:end]
 
-                    # execute training (from 0 to iteration) on mini set
+                    # execute training for step
                     sess.run(train_op, feed_dict={x: X, y: Y})
 
                     if step_number != 0 and (step_number + 1) % 100 == 0:
-                        # where {step_number} is the number of times gradient
+                        # step_number is the number of times gradient
                         # descent has been run in the current epoch
                         print("\tStep {}:".format(step_number + 1))
 
-                        # calculate cost and accuracy for mini set
+                        # calculate cost and accuracy for step
                         step_cost, step_accuracy = sess.run(
                             [loss, accuracy],
                             feed_dict={x: X, y: Y})
 
-                        # where {step_cost} is the cost of the model
+                        # step_cost is the cost of the model
                         # on the current mini-batch
                         print("\t\tCost: {}".format(step_cost))
 
-                        # where {step_accuracy} is the accuracy of the model
+                        # step_accuracy is the accuracy of the model
                         # on the current mini-batch
                         print("\t\tAccuracy: {}".format(step_accuracy))
 
