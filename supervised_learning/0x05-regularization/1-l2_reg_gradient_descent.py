2f3e7c457438f741f870c9b6bd34e974f429a845
felipeserna <feserna86@gmail.com>
2021-01-20 10:10:51 -0500

    it matches the Intranet output

---
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Updates the weights and biases of a neural network using gradient descent
+with L2 regularization
+"""
+
+
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """
+    Updates the weights and biases of a NN using gradient descent
+    with L2 regularization
+    """
+    m = Y.shape[1]
+
+    for i in reversed(range(L)):
+        # if it is the last layer
+        if i == L - 1:
+            dZ = cache['A{}'.format(i + 1)] - Y
+
+        # hidden layers
+        else:
+            dZa = np.matmul(weights['W{}'.format(i + 2)].T, dZ)
+            # derivative of tanh function
+            dZb = 1 - cache['A{}'.format(i + 1)] ** 2
+            dZ = dZa * dZb
+
+            dW = ((np.matmul(dZ, cache['A{}'.format(i)].T)) / m) + \
+                (lambtha / m) * weights['W{}'.format(i + 1)]
+            db = (np.sum(dZ, axis=1, keepdims=True) / m) + \
+                (lambtha / m) * weights['b{}'.format(i + 1)]
+
+            weights['W{}'.format(i + 1)] = \
+                weights['W{}'.format(i + 1)] \
+                - (alpha * dW)
+            weights['b{}'.format(i + 1)] = \
+                weights['b{}'.format(i + 1)] \
+                - (alpha * db)
