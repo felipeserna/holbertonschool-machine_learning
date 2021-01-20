3243b2e42f946180ab35a87bb5d14e0cd91b8c69
felipeserna <feserna86@gmail.com>
2021-01-20 12:12:52 -0500

    creating a copy for weights

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -13,6 +13,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     Updates the weights and biases of a NN using gradient descent
     with L2 regularization
     """
+    weights_c = weights.copy()
     m = Y.shape[1]
 
     for i in reversed(range(L)):
@@ -22,18 +23,18 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
 
         # hidden layers
         else:
-            dZa = np.matmul(weights['W{}'.format(i + 2)].T, dZ)
+            dZa = np.matmul(weights_c['W{}'.format(i + 2)].T, dZ)
             # derivative of tanh function
             dZb = 1 - cache['A{}'.format(i + 1)] ** 2
             dZ = dZa * dZb
 
         dW = ((np.matmul(dZ, cache['A{}'.format(i)].T)) / m) + \
-            (lambtha / m) * weights['W{}'.format(i + 1)]
+            (lambtha / m) * weights_c['W{}'.format(i + 1)]
         db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        weights['W{}'.format(i + 1)] = \
-            weights['W{}'.format(i + 1)] \
+        weights_c['W{}'.format(i + 1)] = \
+            weights_c['W{}'.format(i + 1)] \
             - (alpha * dW)
-        weights['b{}'.format(i + 1)] = \
-            weights['b{}'.format(i + 1)] \
+        weights_c['b{}'.format(i + 1)] = \
+            weights_c['b{}'.format(i + 1)] \
             - (alpha * db)
