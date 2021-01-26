6a2d17021a6ed4bd11b1786e873639e126f4536f
felipeserna <feserna86@gmail.com>
2021-01-26 10:30:06 -0500

    Same as task 5 with early stopping

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Trains a model using mini-batch gradient descent.
+Also analyzes validation data.
+Also trains the model using early stopping
+"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    """
+    Returns: the History object generated after training the model
+    """
+    if validation_data and early_stopping is True:
+        e_s = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+    History = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, callbacks=[e_s],
+                          validation_data=validation_data, shuffle=shuffle)
+    return History
