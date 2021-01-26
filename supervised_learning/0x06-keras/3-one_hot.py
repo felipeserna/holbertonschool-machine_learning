9f597d62f94b37ccc71fd1c0da5e7efb9d8d6426
felipeserna <feserna86@gmail.com>
2021-01-26 06:51:23 -0500

    This project is with Keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -4,7 +4,7 @@ Converts a label vector into a one-hot matrix
 """
 
 
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
@@ -12,6 +12,5 @@ def one_hot(labels, classes=None):
     - The last dimension of the one-hot matrix must be the number of classes
     - Returns: the one-hot matrix
     """
-    oh_matrix = np.zeros((labels.size, labels.max()+1))
-    oh_matrix[np.arange(labels.size), labels] = 1
+    oh_matrix = K.utils.to_categorical(labels, classes)
     return oh_matrix
