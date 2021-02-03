09d5985c5ba61eee7af4e08d7f16e82186359ccc
felipeserna <feserna86@gmail.com>
2021-02-03 10:30:16 -0500

    task 0

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -11,39 +11,44 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
     Returns: the output of the convolutional layer
     """
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
-    sh, sw = stride
+    m = A_prev.shape[0]
+    h_prev = A_prev.shape[1]
+    w_prev = A_prev.shape[2]
 
-    if type(padding) is tuple:
-        ph, pw = padding
+    kh = W.shape[0]
+    kw = W.shape[1]
+    c_new = W.shape[3]
 
-    if padding == "same":
+    sh = stride[0]
+    sw = stride[1]
+
+    if padding == 'same':
         ph = ((h_prev * (sh - 1)) - sh + kh) // 2
         pw = ((w_prev * (sw - 1)) - sw + kw) // 2
-
-    if padding == "valid":
+    if padding == 'valid':
         ph = 0
         pw = 0
 
-    padded_imgs = np.pad(A_prev,
-                         pad_width=((0, 0),
-                                    (ph, ph), (pw, pw),
-                                    (0, 0)),
-                         mode='constant',
-                         constant_values=0)
+    pad_size = ((0, 0), (ph, ph), (pw, pw), (0, 0))
+    images_padded = np.pad(A_prev,
+                           pad_width=pad_size,
+                           mode='constant',
+                           constant_values=0)
 
-    h_output = (padded_imgs.shape[1] - kh)//sh + 1
-    w_output = (padded_imgs.shape[2] - kw)//sw + 1
+    h_new = int(((h_prev - kh + 2 * ph) / sh) + 1)
+    w_new = int(((w_prev - kw + 2 * pw) / sw) + 1)
 
-    convoluted = np.zeros((m, h_output, w_output, c_new))
+    convolved = np.zeros((m, h_new, w_new, c_new))
 
-    for j in range(h_output):
-        for i in range(w_output):
-            for k in range(c_new):
-                convoluted[:, j, i, k] = np.sum(padded_imgs[:,
-                                                            j*sh: j*sh + kh,
-                                                            i*sw: i*sw + kw] *
-                                                W[:, :, :, k], axis=(1, 2, 3))
-    output = activation(convoluted + b)
-    return output
+    for x in range(w_new):
+        for y in range(h_new):
+            for z in range(c_new):
+                convolved[:, y, x, z] = \
+                    (W[:, :, :, z] * images_padded[:,
+                     y * sh: y * sh + kh,
+                     x * sw: x * sw + kw,
+                     :]).sum(axis=(1, 2, 3))
+
+    A_new = activation(convolved + b)
+
+    return A_new
