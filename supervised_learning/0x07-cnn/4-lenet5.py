5ab45684aa775c5d350c790b0dcb1667b198efc0
felipeserna <feserna86@gmail.com>
2021-02-04 14:18:11 -0500

    ian answer

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -36,8 +36,9 @@ def lenet5(x, y):
                              kernel_initializer=init)(flat)
     layer6 = tf.layers.Dense(84, activation='relu',
                              kernel_initializer=init)(layer5)
-    y_pred = tf.layers.Dense(10, activation='softmax',
-                             kernel_initializer=init)(layer6)
+    y_pred = tf.layers.Dense(10, kernel_initializer=init)(layer6)
+
+    y_pred = tf.nn.softmax(y_pred)
     loss = tf.losses.softmax_cross_entropy(y, y_pred)
     grady = tf.train.AdamOptimizer()
     op = grady.minimize(loss)
