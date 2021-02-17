0282c9368a020269a52c6746f6cbee4fc92ce2aa
felipeserna <feserna86@gmail.com>
2021-02-17 13:06:21 -0500

    denseNet121

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -9,41 +9,6 @@ that pre-processes the data for your model
 
 import tensorflow.keras as K
 
-
-if __name__ == "__main__":
-    # Dataset of 50,000 32x32 color training images and 10,000 test images,
-    # labeled over 10 categories
-    (x_train, y_train), (X, Y) = K.datasets.cifar10.load_data()
-
-    # preprocessing
-    x_train_p, y_train_p = preprocess_data(x_train, y_train)
-    x_test_p, y_test_p = preprocess_data(X, Y)
-
-    # InceptionV3
-    #  loads weights pre-trained on ImageNet
-    inc_v3 = K.applications.InceptionV3(include_top=False,
-                                        weights="imagenet",
-                                        input_tensor=None,
-                                        input_shape=None,
-                                        pooling='max')
-
-    model = K.Sequential()
-    model.add(inc_v3)
-    model.add(K.layers.Flatten())
-    model.add(K.layers.Dense(512, activation='relu'))
-    model.add(K.layers.Dense(10, activation='softmax'))
-
-    model.compile(optimizer='adam', loss='categorical_crossentropy'
-                  metrics=['accuracy'])
-
-    # training
-    history = model.fit(x=x_train_p, y=y_train_p,
-                        batch_size=32, epochs=5,
-                        validation_data=(x_test_p, y_test_p))
-
-    model.save('cifar10.h5')
-
-
 def preprocess_data(X, Y):
     """
     Returns: X_p, Y_p
@@ -54,3 +19,50 @@ def preprocess_data(X, Y):
     Y_p = K.utils.to_categorical(Y, num_classes=10)
 
     return X_p, Y_p
+
+if __name__ == "__main__":
+    # Dataset of 50,000 32x32 color training images and 10,000 test images,
+    # labeled over 10 categories
+    (x_train, y_train), (X, Y) = K.datasets.cifar10.load_data()
+    
+    # preprocessing
+    x_train_p, y_train_p = preprocess_data(x_train, y_train)
+    x_test_p, y_test_p = preprocess_data(X, Y)    
+    
+    input = K.Input(shape=(32, 32, 3))
+    input = K.layers.UpSampling2D()(input)
+
+
+    # DenseNet121
+    #  loads weights pre-trained on ImageNet
+    dense_121 = K.applications.DenseNet121(include_top=False,
+                                           weights="imagenet",
+                                           input_tensor=input,
+                                           input_shape=None,
+                                           pooling='max')
+    
+    output = dense_121.layers[-1].output
+    output = K.layers.Flatten()(output)
+    output = K.layers.Dense(512, activation='relu')(output)
+    output = K.layers.Dense(10, activation='softmax')(output)
+
+    model = K.models.Model(dense_121.input, output)
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['acc'])
+
+    # training
+    history = model.fit(x=x_train_p, y=y_train_p,
+                        batch_size=32, epochs=5,
+                        validation_data=(x_test_p, y_test_p),
+                        verbose=1)    
+
+    model.save('cifar10.h5')
+
+# to fix issue with saving keras applications
+K.learning_phase = K.backend.learning_phase 
+
+_, (X, Y) = K.datasets.cifar10.load_data()
+X_p, Y_p = preprocess_data(X, Y)
+model = K.models.load_model('cifar10.h5')
+model.evaluate(X_p, Y_p, batch_size=128, verbose=1)
