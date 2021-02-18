b1e6839e3156d677e1326adcd99e5a48a8d7cc11
felipeserna <feserna86@gmail.com>
2021-02-18 10:01:17 -0500

    val acc over 87%

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -15,7 +15,7 @@ def preprocess_data(X, Y):
     Returns: X_p, Y_p
     """
     # Preprocessing needed in each Keras Application
-    X_p = K.applications.inception_v3.preprocess_input(X)
+    X_p = K.applications.densenet.preprocess_input(X)
     # Converts a label vector into a one-hot matrix
     Y_p = K.utils.to_categorical(Y, num_classes=10)
 
@@ -31,33 +31,37 @@ if __name__ == "__main__":
     x_train_p, y_train_p = preprocess_data(x_train, y_train)
     x_test_p, y_test_p = preprocess_data(X, Y)
 
-    input = K.Input(shape=(32, 32, 3))
-    input = K.layers.UpSampling2D()(input)
-
     # DenseNet121
     #  loads weights pre-trained on ImageNet
-    dense_121 = K.applications.DenseNet121(include_top=False,
-                                           weights="imagenet",
-                                           input_tensor=input,
-                                           input_shape=None,
-                                           pooling='max')
+    dense_121 = K.applications.DenseNet121(weights='imagenet',
+                                           include_top=False,
+                                           input_shape=(224, 224, 3))
 
-    output = dense_121.layers[-1].output
+    dense_121.trainable = False
+
+    input = K.Input(shape=(32, 32, 3))
+    # lambtha layer scales up the data to the correct size
+    lambtha = K.layers.Lambda(lambda X: K.backend.resize_images(X, 7, 7,
+                              data_format="channels_last",
+                              interpolation='bilinear'))(input)
+    output = dense_121(lambtha, training=False)
     output = K.layers.Flatten()(output)
     output = K.layers.Dense(512, activation='relu')(output)
     output = K.layers.Dropout(0.2)(output)
     output = K.layers.Dense(256, activation='relu')(output)
     output = K.layers.Dropout(0.2)(output)
+    output = K.layers.Dense(128, activation='relu')(output)
+    output = K.layers.Dropout(0.2)(output)
     output = K.layers.Dense(10, activation='softmax')(output)
 
-    model = K.models.Model(dense_121.input, output)
+    model = K.Model(input, output)
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['acc'])
 
     # training
     history = model.fit(x=x_train_p, y=y_train_p,
-                        batch_size=256, epochs=20,
+                        batch_size=128, epochs=5,
                         validation_data=(x_test_p, y_test_p),
                         verbose=1)
 
