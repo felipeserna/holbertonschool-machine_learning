7d96d22eba90a8460f1a07ec31e40bb35bcce7a0
felipeserna <feserna86@gmail.com>
2021-03-22 18:31:10 -0500

    mean and covariance

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Calculates the mean and covariance of a data set
+"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Returns: mean, cov
+    """
+    n = len(X)
+    d = len(X[0])
+
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        TypeError("X must be a 2D numpy.ndarray")
+
+    if n < 2:
+        ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0).reshape(1, d)
+
+    cov = np.matmul((X - mean).T, (X - mean)*(1/(n - 1)))
+
+    return mean, cov
