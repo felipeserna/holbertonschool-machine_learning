0bab5f8fecf64d49416614e7c49bb9e2ce2e76ad
felipeserna <feserna86@gmail.com>
2021-03-22 19:09:40 -0500

    mean and covariance

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python3
 """
-Module 0-mean_cov
-Contains function mean_cov
+Calculates the mean and covariance of a data set
 """
+
+
 import numpy as np
 
 
 def mean_cov(X):
     """
-    Function that calculates the mean and covariance of a data set.
-    Args:
-    X (numpy.ndarray): with shape (n, d) contains the data set:
-        n (int): is the number of data points
-        d (int): is the number of dimensions in each data point
-    Returns:
-    mean, cov:
-        mean (numpy.ndarray): with shape (1, d) contains
-            the mean of the data set
-        cov (numpy.ndarray): with shape (d, d) containing
-            the covariance matrix of the data set
+    Returns: mean, cov
     """
-
-    if type(X) is not np.ndarray or len(X.shape) is not 2:
-        raise TypeError('X must be a 2D numpy.ndarray')
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
 
     n = X.shape[0]
+
     if n < 2:
-        raise ValueError('X must contain multiple data points')
+        raise ValueError("X must contain multiple data points")
 
     d = X.shape[1]
+
     mean = np.mean(X, axis=0).reshape(1, d)
 
-    deviaton = X - mean
+    deviation = X - mean
 
-    cov = np.matmul(deviaton.T, deviaton) / (n - 1)
+    cov = np.matmul(deviation.T, deviation) / (n - 1)
+
     return mean, cov
