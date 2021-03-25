f0063afc87bf454ad08097234315b141166b5283
felipeserna <feserna86@gmail.com>
2021-03-25 12:07:58 -0500

    skeleton

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Calculates the likelihood of obtaining this data
+given various hypothetical probabilities of developing severe side effects
+"""
+
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    Returns: a 1D numpy.ndarray containing the likelihood
+    of obtaining the data, x and n, for each probability in P, respectively
+    """
+    return None
