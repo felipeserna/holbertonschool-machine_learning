fe904eaf6a73cb50f21a9048bee5419bf7b91c7c
felipeserna <feserna86@gmail.com>
2021-03-24 19:55:15 -0500

    P affinities

--- unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -5,6 +5,8 @@ Calculates the symmetric P affinities of a data set
 
 
 import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
