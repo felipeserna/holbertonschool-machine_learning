80e6b318c03eed87fe06063006e7d6c6d5de63f7
felipeserna <feserna86@gmail.com>
2021-03-24 19:48:31 -0500

    P affinities

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Calculates the symmetric P affinities of a data set
+"""
+
+
+import numpy as np
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """
+    Returns: P, a numpy.ndarray of shape (n, n)
+    containing the symmetric P affinities
+    """
+    return None
