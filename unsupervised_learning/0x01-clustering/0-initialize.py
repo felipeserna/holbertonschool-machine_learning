46d705fe74b66fe8c88174d8ff2ff49864e3c96b
felipeserna <feserna86@gmail.com>
2021-03-28 17:38:27 -0500

    skeleton

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Initializes cluster centroids for K-means
+"""
+
+
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    Returns: a numpy.ndarray of shape (k, d)
+    containing the initialized centroids for each cluster, or None on failure
+    """
+    
