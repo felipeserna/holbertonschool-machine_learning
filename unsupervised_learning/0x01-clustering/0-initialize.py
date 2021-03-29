964feacef4eedc3294d62525a91860500f8d3a17
felipeserna <feserna86@gmail.com>
2021-03-29 12:26:02 -0500

    Initialize K-means

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -12,7 +12,10 @@ def initialize(X, k):
     Returns: a numpy.ndarray of shape (k, d)
     containing the initialized centroids for each cluster, or None on failure
     """
-    if k <= 0:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+
+    if type(k) is not int or k <= 0 or k >= X.shape[0]:
         return None
 
     d = X.shape[1]
@@ -20,9 +23,6 @@ def initialize(X, k):
     maxi = np.amax(X, axis=0)
 
     # Initialize cluster centroids
-    try:
-        init = np.random.uniform(mini, maxi, size=(k, d))
-    except Exception:
-        return None
+    init = np.random.uniform(mini, maxi, size=(k, d))
 
     return init
