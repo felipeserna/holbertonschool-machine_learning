96b909c9c6344cf6a4581ed06d107a83caf384bd
felipeserna <feserna86@gmail.com>
2021-03-30 16:38:24 -0500

    Variance

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -20,7 +20,7 @@ def variance(X, C):
 
     # https://www.youtube.com/watch?v=xNfOheh-res&ab_channel=VictorLavrenko
 
-    dist = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
+    dist = np.linalg.norm(X - C[:, np.newaxis], axis=2)
     min_dist = np.min(dist, axis=0)
     intra_var = np.sum(min_dist**2)
     total_var = np.sum(intra_var)
