df5a3d711e97a69a2b3fb74e1c4a38485ede768a
felipeserna <feserna86@gmail.com>
2021-03-29 20:30:09 -0500

    K-means

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -8,6 +8,27 @@ Performs K-means on a dataset.
 import numpy as np
 
 
+def initialize(X, k):
+    """
+    Returns: a numpy.ndarray of shape (k, d)
+    containing the initialized centroids for each cluster, or None on failure
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+
+    if type(k) is not int or k <= 0 or k >= X.shape[0]:
+        return None
+
+    d = X.shape[1]
+    mini = np.amin(X, axis=0)
+    maxi = np.amax(X, axis=0)
+
+    # Initialize cluster centroids
+    init = np.random.uniform(mini, maxi, size=(k, d))
+
+    return init
+
+
 def kmeans(X, k, iterations=1000):
     """
     Returns: C, clss, or None, None on failure
@@ -21,11 +42,29 @@ def kmeans(X, k, iterations=1000):
     if type(iterations) is not int or iterations <= 0:
         return None, None
 
-    d = X.shape[1]
-    mini = np.amin(X, axis=0)
-    maxi = np.amax(X, axis=0)
+    C = initialize(X, k)
+    clss = None
+    for _ in range(iterations):
+        C_copy = np.copy(C)
+        dist = np.linalg.norm(X[:, np.newaxis] - C, axis=-1)
+        # minimum distance
+        clss = np.argmin(dist, axis=-1)
+        # move the centroids
+        for j in range(k):
+            index = np.argwhere(clss == j)
+            # If a cluster contains no data points during the update step,
+            # reinitialize its centroid
+            if not len(index):
+                C[j] = initialize(X, 1)
+            else:
+                C[j] = np.mean(X[index], axis=0)
 
-    # Initialize cluster centroids
-    init = np.random.uniform(mini, maxi, size=(k, d))
+        # If no change in the cluster centroids occurs between iterations,
+        # return
+        if (C_copy == C).all():
+            return C, clss
 
-    return init
+    dist = np.linalg.norm(X[:, np.newaxis] - C, axis=-1)
+    clss = np.argmin(dist, axis=-1)
+
+    return C, clss
