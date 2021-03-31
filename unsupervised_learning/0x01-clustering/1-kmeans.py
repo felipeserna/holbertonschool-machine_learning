ea905d2c1e4181accfc177038bae78e6dc1460aa
felipeserna <feserna86@gmail.com>
2021-03-31 12:05:15 -0500

    kmeans

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -1,35 +1,23 @@
 #!/usr/bin/env python3
 """
-Function K-means
+Initializes cluster centroids for K-means.
+Performs K-means on a dataset.
 """
 import numpy as np
 
 
 def kmeans(X, k, iterations=1000):
     """
-    Function that performs K-means on a dataset
-    Arguments:
-     - X is a numpy.ndarray of shape (n, d) containing the dataset
-        * n is the number of data points
-        * d is the number of dimensions for each data point
-     - k is a positive integer containing the number of clusters
-     - iterations is a positive integer containing the maximum number of
-        iterations that should be performed
-    Returns:
-     C, clss, or None, None on failure
-         - C is a numpy.ndarray of shape (k, d) containing the centroid means
-            for each cluster
-         - clss is a numpy.ndarray of shape (n,) containing the index of the
-            cluster in C that each data point belongs to
+    Returns: C, clss, or None, None on failure
     """
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
 
-    if type(k) != int or k <= 0:
+    if type(k) is not int or k <= 0:
         return None, None
 
-    if type(iterations) != int or iterations <= 0:
+    if type(iterations) is not int or iterations <= 0:
         return None, None
 
     n, d = X.shape
@@ -37,26 +25,32 @@ def kmeans(X, k, iterations=1000):
     minimum = np.amin(X, axis=0)
     maximum = np.amax(X, axis=0)
 
-    # C = np.random.uniform(minimum, maximum, (k, d))
     C = initialize(X, k)
     clss = None
-    for i in range(iterations):
-        C_cpy = np.copy(C)
-        distance = np.linalg.norm(X[:, None] - C, axis=-1)
-        clss = np.argmin(distance, axis=-1)
+    for _ in range(iterations):
+        C_copy = np.copy(C)
+        # new axis for broadcasting
+        dist = np.linalg.norm(X[:, np.newaxis] - C, axis=-1)
+        # minimum distance
+        clss = np.argmin(dist, axis=-1)
         # move the centroids
         for j in range(k):
             index = np.argwhere(clss == j)
+            # If a cluster contains no data points during the update step,
+            # reinitialize its centroid
             if not len(index):
                 C[j] = initialize(X, 1)
             else:
                 C[j] = np.mean(X[index], axis=0)
 
-        if (C_cpy == C).all():
+        # If no change in the cluster centroids occurs between iterations,
+        # return
+        if (C_copy == C).all():
             return C, clss
 
-    distance = np.linalg.norm(X[:, None] - C, axis=-1)
-    clss = np.argmin(distance, axis=-1)
+    # new axis for broadcasting
+    dist = np.linalg.norm(X[:, np.newaxis] - C, axis=-1)
+    clss = np.argmin(dist, axis=-1)
 
     return C, clss
 
