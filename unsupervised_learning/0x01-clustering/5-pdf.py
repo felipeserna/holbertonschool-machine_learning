436e4e2ea28bba4323c9047f7af5f9a45b9b8aaa
felipeserna <feserna86@gmail.com>
2021-03-31 17:01:31 -0500

    pdf

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
 """
-module 5-pdf
-contains function pdf
+Calculates the probability density function of a Gaussian distribution
 """
+
+
 import numpy as np
 
 
 def pdf(X, m, S):
-    """Calculates the probability density function of
-    a Gaussian distribution
     """
+    Returns: P, or None on failure
+    - P is a numpy.ndarray of shape (n,)
+      containing the PDF values for each data point
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
 
-    if type(X) is not np.ndarray or len(X.shape) is not 2:
-        return None
-    if type(m) is not np.ndarray or len(m.shape) is not 1:
-        return None
-    if type(S) is not np.ndarray or len(S.shape) is not 2:
+    if type(m) is not np.ndarray or len(m.shape) != 1:
         return None
 
-    _, d = X.shape
+    if type(S) is not np.ndarray or len(S.shape) != 2:
+        return None
+
+    d = X.shape[1]
+
     if d != m.shape[0] or d != S.shape[0]:
         return None
+
     if S.shape[0] != S.shape[1] or d != S.shape[1]:
         return None
 
-    den = (((2 * np.pi) ** d) * np.linalg.det(S))**(1/2)
-    expo = (-0.5 * np.sum(np.matmul(np.linalg.inv(S),
+    cov_det = np.linalg.det(S)
+    cov_inv = np.linalg.inv(S)
+
+    # denominator
+    den = np.sqrt(((2 * np.pi) ** d) * cov_det)
+
+    # exponential term
+    expo = (-0.5 * np.sum(np.matmul(cov_inv,
             (X.T - m[:, np.newaxis])) *
             (X.T - m[:, np.newaxis]), axis=0))
-    ans = np.exp(expo) / den
-    ans = np.where(ans < 1e-300, 1e-300, ans)
-    return ans
+
+    P = np.exp(expo) / den
+    P = np.where(P < 1e-300, 1e-300, P)
+    return P
