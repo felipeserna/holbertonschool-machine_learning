806bbafcd8f282eec6051370a41b4b96d3d23338
felipeserna <feserna86@gmail.com>
2021-03-30 20:14:49 -0500

    Initialize GMM

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Initializes variables for a Gaussian Mixture Model
+"""
+
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    Returns: pi, m, S, or None, None, None on failure
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None
+
+    if type(k) is not int or k < 0:
+        return None, None, None
+
+    if k > X.shape[0]:
+        return None, None, None
+
+    d = X.shape[1]
+
+    pi = (1 / k) * np.ones(k)
+    m, _ = kmeans(X, k)
+    S = np.full((k, d, d), np.identity(d))
+    return pi, m, S
