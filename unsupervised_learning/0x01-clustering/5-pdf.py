e29a760767c0baebaf2d1084da869bd3c82251fc
felipeserna <feserna86@gmail.com>
2021-03-31 12:54:48 -0500

    pdf

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Calculates the probability density function of a Gaussian distribution
+"""
+
+
+import numpy as np
+
+
+def pdf(X, m, S):
+    """
+    Returns: P, or None on failure
+    - P is a numpy.ndarray of shape (n,)
+      containing the PDF values for each data point
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+
+    return None
