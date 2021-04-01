f3c7a9396c9914ad56c41a0985d5bc6e5d214aae
felipeserna <feserna86@gmail.com>
2021-03-31 19:27:30 -0500

    expectation

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -18,26 +18,37 @@ def expectation(X, pi, m, S):
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
 
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
+
     if type(pi) is not np.ndarray or len(pi.shape) != 1:
         return None, None
 
-    k = pi.shape[0]
+    n, d = X.shape
 
-    if type(m) is not np.ndarray or len(m.shape) != 1:
+    if d != S.shape[1] or S.shape[1] != S.shape[2]:
+        return (None, None)
+
+    if d != m.shape[1] or m.shape[0] != S.shape[0]:
+        return (None, None)
+
+    if pi.shape[0] != m.shape[0]:
+        return (None, None)
+
+    # Priors must sum to 1
+    if not np.isclose(np.sum(pi), 1):
         return None, None
 
-    if k != m.shape[0]:
-        return None, None
+    k = S.shape[0]
+    tmp = np.zeros((k, n))
 
-    if type(S) is not np.ndarray or len(S.shape) != 2:
-        return None, None
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        tmp[i] = pi[i] * P
+    g = tmp / np.sum(tmp, axis=0)
+    total_log_like = np.sum(np.log(np.sum(tmp, axis=0)))
 
-    d = X.shape[1]
-
-    if d != m.shape[0] or d != S.shape[0]:
-        return None, None
-
-    if S.shape[0] != S.shape[1] or d != S.shape[1]:
-        return None, None
-
-    return None, None
+    return g, total_log_like
