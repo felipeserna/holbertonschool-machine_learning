3c7dbf587fb552f68fc087395e8e7fb4602225a9
felipeserna <feserna86@gmail.com>
2021-04-02 15:23:13 -0500

    EM

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
-Performs the expectation maximization for a GMM
+module 8-EM
+contains function expectation_maximization
 """
-
-
 import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
@@ -11,45 +10,36 @@ maximization = __import__('7-maximization').maximization
 
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
-    """
-    Returns: pi, m, S, g, l, or None, None, None, None, None on failure
-    """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+    """Performs the expectation maximization for a GMM"""
+    if type(X) is not np.ndarray or len(X.shape) is not 2:
         return None, None, None, None, None
-
-    if type(k) is not int or k < 0 or X.shape[0] < k:
+    if type(k) is not int or k <= 0 or k >= X.shape[0]:
         return None, None, None, None, None
-
     if type(iterations) is not int or iterations <= 0:
         return None, None, None, None, None
-
-    if type(tol) is not float or tol < 0:
+    if type(tol) is not float or tol <= 0:
         return None, None, None, None, None
-
     if type(verbose) is not bool:
         return None, None, None, None, None
 
     pi, m, S = initialize(X, k)
-
-    prev_like = 0
-
-    g, total_log_like = expectation(X, pi, m, S)
-
-    text = "Log Likelihood after {} iterations: {}"
+    g, lklhd = expectation(X, pi, m, S)
+    prev_lklhd = 0
 
     for i in range(iterations):
-        if verbose and i % 10 == 0:
-            print(text.format(i, total_log_like.round(5)))
+        if (i % 10 == 0) and verbose is True:
+            print('Log Likelihood after {} iterations: {}'.
+                  format(i, lklhd.round(5)))
 
         pi, m, S = maximization(X, g)
-        g, total_log_like = expectation(X, pi, m, S)
+        g, lklhd = expectation(X, pi, m, S)
 
-        if abs(prev_like - total_log_like) <= tol:
+        if tol >= abs(lklhd - prev_lklhd):
             break
+        prev_lklhd = lklhd
 
-        prev_like = total_log_like
+    if verbose is True:
+        print('Log Likelihood after {} iterations: {}'.
+              format(i + 1, lklhd.round(5)))
 
-    if verbose:
-        print(text.format(i + 1, total_log_like.round(5)))
-
-    return pi, m, S, g, total_log_like
+    return pi, m, S, g, lklhd
