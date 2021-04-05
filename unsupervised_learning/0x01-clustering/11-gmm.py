86a04314e842e95e46e4d96fd0aa90ebca2a6917
felipeserna <feserna86@gmail.com>
2021-04-04 20:05:56 -0500

    descriptions for methods and attributes

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -12,11 +12,15 @@ def gmm(X, k):
     Returns: pi, m, S, clss, bic
     """
     GMM = sklearn.mixture.GaussianMixture(n_components=k)
+    # Estimate model parameters with the EM algorithm.
     GMM.fit(X)
     pi = GMM.weights_
     m = GMM.means_
     S = GMM.covariances_
+    # Predict the labels for the data samples in X using trained model.
     clss = GMM.predict(X)
+    # Bayesian information criterion for the current model on the input X.
+    # The lower the better.
     bic = GMM.bic(X)
 
     return pi, m, S, clss, bic
