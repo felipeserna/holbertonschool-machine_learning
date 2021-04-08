431c07937e4b6b58f4f21bc7c40114c43cfb3eb2
felipeserna <feserna86@gmail.com>
2021-04-07 20:32:47 -0500

    Edge cases

--- unsupervised_learning/0x02-hmm/0-markov_chain.py
+++ unsupervised_learning/0x02-hmm/0-markov_chain.py
@@ -14,4 +14,13 @@ def markov_chain(P, s, t=1):
     the probability of being in a specific state
     after t iterations, or None on failure
     """
-    
+    if len(P.shape) != 2 or P.shape[0] != P.shape[1] or P.shape[0] < 1:
+        return None
+
+    if len(s.shape) != 2 or s.shape[1] != P.shape[0]:
+        return None
+
+    if type(t) is not int or t < 1:
+        return None
+
+    return None
