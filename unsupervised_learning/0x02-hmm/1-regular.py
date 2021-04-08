53f3b5cde084ac5a438c048aea57e09feb91f6b6
felipeserna <feserna86@gmail.com>
2021-04-08 11:28:56 -0500

    Regular Chains

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -15,4 +15,10 @@ def regular(P):
     if len(P.shape) != 2 or P.shape[0] != P.shape[1] or P.shape[0] < 1:
         return None
 
-    return None
+    P = np.linalg.matrix_power(P, 100)
+
+    if np.any(P <= 0):
+        return None
+
+    steady_state = np.array([P[0]])
+    return steady_state
