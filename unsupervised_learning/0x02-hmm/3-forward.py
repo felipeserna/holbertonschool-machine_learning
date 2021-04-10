b202a248370a989d651a08f5859a02e78b3a4b74
felipeserna <feserna86@gmail.com>
2021-04-10 12:33:19 -0500

    The Forward Algorithm

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -14,31 +14,48 @@ def forward(Observation, Emission, Transition, Initial):
     if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
         return None, None
 
-    if Observation.shape[0] <= 0:
-        return None, None
-
     if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-
-    if Emission.shape[0] <= 0 or Emission.shape[1] <= 0:
         return None, None
 
     if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
         return None, None
 
-    if Transition.shape[0] != Transition.shape[1]:
-        return None, None
-
-    if Emission.shape[0] != Transition.shape[0]:
-        return None, None
-
     if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
         return None, None
 
-    if Initial.shape[0] != Transition.shape[0] or Initial.shape[1] != 1:
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
         return None, None
 
-    if Initial.shape[1] != 1:
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
         return None, None
 
-    return None, None
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    F = np.zeros((N, T))
+
+    # Initialization
+    Obs_i = Observation[0]
+    F[:, 0] = np.multiply(Initial[:, 0], Emission[:, Obs_i])
+
+    # Recursion
+    for i in range(1, T):
+        Obs_i = Observation[i]
+        state = np.matmul(F[:, i - 1], Transition)
+        F[:, i] = np.multiply(state, Emission[:, Obs_i])
+
+    # Sum of path probabilities over all possible states
+    # end of path
+    P = np.sum(F[:, T - 1])
+
+    return P, F
