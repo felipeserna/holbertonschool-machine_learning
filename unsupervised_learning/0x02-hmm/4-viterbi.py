d6e8c5c61b1497f2b15f2d72d356061c448e73ac
felipeserna <feserna86@gmail.com>
2021-04-10 16:47:29 -0500

    The Viterbi Algorithm

--- unsupervised_learning/0x02-hmm/4-viterbi.py
+++ unsupervised_learning/0x02-hmm/4-viterbi.py
@@ -14,28 +14,68 @@ def viterbi(Observation, Emission, Transition, Initial):
     if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
         return None, None
 
-    if Observation.shape[0] <= 0:
-        return None, None
-
     if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
-        return None, None
-
-    if Emission.shape[0] <= 0 or Emission.shape[1] <= 0:
         return None, None
 
     if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
         return None, None
 
-    if Transition.shape[0] != Transition.shape[1]:
-        return None, None
-
-    if Emission.shape[0] != Transition.shape[0]:
-        return None, None
-
     if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
         return None, None
 
-    if Initial.shape[0] != Transition.shape[0] or Initial.shape[1] != 1:
+    T = Observation.shape[0]
+
+    N, _ = Emission.shape
+
+    if Transition.shape[0] != N or Transition.shape[1] != N:
         return None, None
 
-    return None, None
+    if Initial.shape[0] != N or Initial.shape[1] != 1:
+        return None, None
+
+    if not np.sum(Emission, axis=1).all():
+        return None, None
+
+    if not np.sum(Transition, axis=1).all():
+        return None, None
+
+    if not np.sum(Initial) == 1:
+        return None, None
+
+    viterbi = np.zeros((N, T))
+    backpointer = np.zeros((N, T))
+
+    # Initialization
+    Obs_t = Observation[0]
+    backpointer[:, 0] = 0
+    prob = np.multiply(Initial[:, 0], Emission[:, Obs_t])
+    viterbi[:, 0] = prob
+
+    # Recursion
+    for t in range(1, T):
+        a = viterbi[:, t - 1]
+        b = Transition.T
+        ab = a * b
+        ab_max = np.amax(ab, axis=1)
+        c = Emission[:, Observation[t]]
+        prob = ab_max * c
+
+        viterbi[:, t] = prob
+        backpointer[:, t - 1] = np.argmax(ab, axis=1)
+
+    # Path initialization
+    path = []
+    current = np.argmax(viterbi[:, T - 1])
+    path = [current] + path
+
+    # Path backwards traversing
+    for t in range(T - 2, -1, -1):
+        current = int(backpointer[current, t])
+        path = [current] + path
+
+    # max path probabilities among all possible states
+    # end of path
+
+    P = np.amax(viterbi[:, T - 1], axis=0)
+
+    return path, P
