a46200aec75c256e0e9b877651ad542b3082b006
felipeserna <feserna86@gmail.com>
2021-04-17 17:32:48 -0500

    Bayesian Optimization with GPyOpt

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/6-bayes_opt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Optimizes a ML model of your choice using GPyOpt
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import GPy
+import GPyOpt
+
+from GPyOpt.methods import BayesianOptimization
+
+
+if __name__ == '__main__':
+    pass
