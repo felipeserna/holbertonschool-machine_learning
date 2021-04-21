ec3e59c3bd9f60bf2537fd9ef2d914bc10362b78
felipeserna <feserna86@gmail.com>
2021-04-21 10:18:41 -0500

    skeleton vanilla autoencoder

---
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creates an autoencoder
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Returns: encoder, decoder, auto
+    """
+    return None, None, None
