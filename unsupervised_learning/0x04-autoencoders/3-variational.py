c138f99b8d028db3882ab010031a5024008d02f6
felipeserna <feserna86@gmail.com>
2021-04-22 19:52:29 -0500

    skeleton

---
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creates a variational autoencoder
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Returns: encoder, decoder, auto
+    """
+    return None, None, None
