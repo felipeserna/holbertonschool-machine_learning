c248893d3d265f5b6e0f7078697aa50b9825f6ed
felipeserna <feserna86@gmail.com>
2021-04-25 16:42:34 -0500

    variational autoencoder

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -6,24 +6,8 @@ import tensorflow.keras as keras
 
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
-    """Function that creates a variational autoencoder. The autoencoder model
-    is compiled using adam optimization and binary cross-entropy loss.
-    All layers use a relu activation except for the mean and log variance
-    layers in the encoder, which uses None, and the last layer in the decoder,
-    which uses sigmoid.
-    Arguments:
-        input_dims: `int`, dimensions of the model input.
-        hidden_layers: `list`, with the number of nodes
-            for each hidden layer in the encoder, respectively.
-            the hidden layers should be reversed for the decoder.
-        latent_dims: `int`, dimensions of the latent space representation.
-    Returns:
-        encoder, decoder, auto: `tuple`,
-            encoder is the encoder model, which outputs the latent
-                representation, the mean, and the log variance,
-                respectively.
-            decoder is the decoder model.
-            auto is the full autoencoder model.
+    """
+    Returns: encoder, decoder, auto
     """
     # This is our input placeholder
     inputs = keras.Input(shape=(input_dims, ))
@@ -49,14 +33,15 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         dims = keras.backend.int_shape(z_mean)[1]
         epsilon = keras.backend.random_normal(shape=(batch, dims))
         z = z_mean + keras.backend.exp(0.5 * z_log_sigma) * epsilon
+
         return z
 
     z = keras.layers.Lambda(sampling,
                             output_shape=(latent_dims, ))([z_mean,
                                                            z_log_sigma])
 
-    # This is our encoded input placeholder
     encoded_inputs = keras.Input(shape=(latent_dims, ))
+
     decoder_layers = keras.layers.Dense(units=hidden_layers[-1],
                                         activation='relu')(encoded_inputs)
 
@@ -81,14 +66,13 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     code = encoder(inputs)[0]
     outputs = decoder(code)
 
-    # This model generates new instarnces that look like that they were
+    # This model generates new instances that look like that they were
     # sampled from the training set
     auto = keras.models.Model(inputs=inputs, outputs=outputs,
                               name="autoencoder")
 
     def variational_autoencoder_loss(inputs, outputs):
         """
-        variational autoencoder loss function implementation
         https://blog.keras.io/building-autoencoders-in-keras.html
         """
         reconstruction_loss = keras.losses.binary_crossentropy(inputs, outputs)
@@ -98,6 +82,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         kl_loss = keras.backend.sum(kl_loss, axis=-1)
         kl_loss *= -0.5
         vae_loss = keras.backend.mean(reconstruction_loss + kl_loss)
+
         return vae_loss
 
     auto.compile(optimizer='Adam', loss=variational_autoencoder_loss)
