f99e2561d88bdbfe13ff6189d1010afd38496993
felipeserna <feserna86@gmail.com>
2021-05-08 12:48:20 -0500

    preprocess_data.py

--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -2,3 +2,20 @@
 """
 Preprocess raw dataset
 """
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# making data frame from csv file
+data = \
+    pd.read_csv('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv')
+
+data = data[0::60]
+# creating bool series True for Not NaN values
+bool_series = pd.notnull(data["Open"])
+
+# displaying data only with Open = Not NaN
+new_data = data[bool_series]
+converted_df = pd.to_datetime(new_data["Timestamp"], unit="s")
+# new_data = new_data[convert]
+print(converted_df)
