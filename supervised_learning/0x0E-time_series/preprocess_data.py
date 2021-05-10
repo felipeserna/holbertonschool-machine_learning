138d282fa89b97af81c7a42b97d6f319b358c4ef
felipeserna <feserna86@gmail.com>
2021-05-09 19:21:52 -0500

    Timestamp and Close

--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -10,12 +10,16 @@ import matplotlib.pyplot as plt
 data = \
     pd.read_csv('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv')
 
+data = data.drop(["Open", "High", "Low", "Volume_(BTC)",
+                 "Volume_(Currency)", "Weighted_Price"], axis=1)
+
 data = data[0::60]
 # creating bool series True for Not NaN values
-bool_series = pd.notnull(data["Open"])
+# bool_series = pd.notnull(data["Open"])
 
 # displaying data only with Open = Not NaN
-new_data = data[bool_series]
-converted_df = pd.to_datetime(new_data["Timestamp"], unit="s")
+# new_data = data[bool_series]
+# converted_df = pd.to_datetime(new_data["Timestamp"], unit="s")
 # new_data = new_data[convert]
-print(converted_df)
+# print(converted_df)
+print(data)
