758b66dbdaf3b1de08a0abbd7d67fe9d829b66d2
felipeserna <feserna86@gmail.com>
2021-05-11 15:26:03 -0500

    skeleton

---
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creates a bag of words embedding matrix
+"""
+import numpy as np
+
+
+def bag_of_words(sentences, vocab=None):
+    """
+    Returns: embeddings, features
+    """
+    return None
