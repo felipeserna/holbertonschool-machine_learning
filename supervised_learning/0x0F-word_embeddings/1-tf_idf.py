c5f46e02841b12bf4e3d46b908267a2f23aa9f24
felipeserna <feserna86@gmail.com>
2021-05-11 15:28:43 -0500

    skeleton

---
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Creates a TF-IDF embedding
+"""
+import numpy as np
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Returns: embeddings, features
+    """
+    return None
