df4f1ec7a30f6df526ca2575990fa29a293ab03f
felipeserna <feserna86@gmail.com>
2021-05-13 09:09:38 -0500

    skeleton

---
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Calculates the n-gram BLEU score for a sentence
+"""
+import numpy as np
+
+
+def ngram_bleu(references, sentence, n):
+    """
+    Returns: the n-gram BLEU score
+    """
+    return 0.0
