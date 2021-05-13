0d8f0af13a5116118a17cf1946631c9465f2810b
felipeserna <feserna86@gmail.com>
2021-05-13 09:05:25 -0500

    skeleton

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Calculates the unigram BLEU score for a sentence
+"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """
+    Returns: the unigram BLEU score
+    """
+    return None
