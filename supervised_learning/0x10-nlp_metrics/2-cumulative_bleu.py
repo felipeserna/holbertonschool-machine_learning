91497e3a1c00c0c50e9d93a0570eceb75f7f61d6
felipeserna <feserna86@gmail.com>
2021-05-13 09:12:42 -0500

    skeleton

---
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Calculates the cumulative n-gram BLEU score for a sentence
+"""
+import numpy as np
+
+
+def cumulative_bleu(references, sentence, n):
+    """
+    Returns: the cumulative n-gram BLEU score
+    """
+    return 0.6
