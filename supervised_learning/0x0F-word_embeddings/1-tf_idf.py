9c29ebcd187146a020435578312ff8fb29296825
felipeserna <feserna86@gmail.com>
2021-05-16 18:02:33 -0500

    TF - IDF

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -2,11 +2,19 @@
 """
 Creates a TF-IDF embedding
 """
-import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def tf_idf(sentences, vocab=None):
     """
     Returns: embeddings, features
     """
-    return None
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+
+    X = vectorizer.fit_transform(sentences)
+
+    features = vectorizer.get_feature_names()
+
+    embeddings = X.toarray()
+
+    return embeddings, features
