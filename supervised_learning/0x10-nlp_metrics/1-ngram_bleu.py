35bb0d4e6bafad6e96b6d5d813a9fcc8e0fe9b94
felipeserna <feserna86@gmail.com>
2021-05-16 20:11:40 -0500

    N-gram BLEU score

--- supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -9,4 +9,33 @@ def ngram_bleu(references, sentence, n):
     """
     Returns: the n-gram BLEU score
     """
-    return 0.0
+    len_refer = []
+    clipped = {}
+
+    N_sentence = [' '.join([str(jd) for jd in sentence[id:id + n]])
+                  for id in range(len(sentence) - (n - 1))]
+    len_Noutput = (len(N_sentence))
+
+    for refs in references:
+        N_reference = [' '.join([str(jd) for jd in refs[id:id + n]])
+                       for id in range(len(sentence) - (n - 1))]
+
+        len_refer.append(len(refs))
+
+        for w in N_reference:
+            if w in N_sentence:
+                if not clipped.keys() == w:
+                    clipped[w] = 1
+
+    clipped_count = sum(clipped.values())
+    closest_idx = np.argmin([abs(len(x) - len_Noutput) for x in references])
+    closest_len_refer = len(references[closest_idx])
+
+    if len_Noutput > closest_len_refer:
+        bp = 1
+    else:
+        bp = np.exp(1 - (closest_len_refer / len(sentence)))
+
+    BLEU_score = bp * np.exp(np.log(clipped_count / len_Noutput))
+
+    return BLEU_score
