c420f65536b5c4ecf561e397503bdd5374b86bed
felipeserna <feserna86@gmail.com>
2021-05-16 20:19:56 -0500

    N-gram BLEU score

--- supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -5,37 +5,90 @@ Calculates the n-gram BLEU score for a sentence
 import numpy as np
 
 
+def grams(sentence, n):
+    """
+    Get grams
+    """
+    new = []
+    len_sentence = len(sentence)
+
+    for i, word in enumerate(sentence):
+        s = word
+        counter = 0
+        j = 1
+
+        for j in range(1, n):
+            if len_sentence > i + j:
+                s += " " + sentence[i + j]
+                counter += 1
+        if counter == j:
+            new.append(s)
+    return new
+
+
+def transform_grams(references, sentence, n):
+    """
+    Returns: new_ref, new_sentence
+    """
+    if n == 1:
+        return references, sentence
+
+    new_sentence = grams(sentence, n)
+    new_ref = []
+
+    for ref in references:
+        new_r = grams(ref, n)
+        new_ref.append(new_r)
+
+    return new_ref, new_sentence
+
+
+def calc_precision(references, sentence, n):
+    """
+    Returns: precision
+    """
+    references, sentence = transform_grams(references, sentence, n)
+    sentence_dict = {x: sentence.count(x) for x in sentence}
+    references_dict = {}
+
+    for ref in references:
+        for gram in ref:
+            if gram not in references_dict.keys() \
+                    or references_dict[gram] < ref.count(gram):
+
+                references_dict[gram] = ref.count(gram)
+
+    appearances = {x: 0 for x in sentence}
+
+    for ref in references:
+        for gram in appearances.keys():
+            if gram in ref:
+                appearances[gram] = sentence_dict[gram]
+
+    for gram in appearances.keys():
+        if gram in references_dict.keys():
+            appearances[gram] = min(references_dict[gram], appearances[gram])
+
+    len_trans = len(sentence)
+    precision = sum(appearances.values()) / len_trans
+
+    return precision
+
+
 def ngram_bleu(references, sentence, n):
     """
     Returns: the n-gram BLEU score
     """
-    len_refer = []
-    clipped = {}
+    precision = calc_precision(references, sentence, n)
+    len_trans = len(sentence)
+    ind = np.argmin([abs(len(x) - len_trans) for x in references])
+    reference_length = len(references[ind])
 
-    N_sentence = [' '.join([str(jd) for jd in sentence[id:id + n]])
-                  for id in range(len(sentence) - (n - 1))]
-    len_Noutput = (len(N_sentence))
-
-    for refs in references:
-        N_reference = [' '.join([str(jd) for jd in refs[id:id + n]])
-                       for id in range(len(sentence) - (n - 1))]
-
-        len_refer.append(len(refs))
-
-        for w in N_reference:
-            if w in N_sentence:
-                if not clipped.keys() == w:
-                    clipped[w] = 1
-
-    clipped_count = sum(clipped.values())
-    closest_idx = np.argmin([abs(len(x) - len_Noutput) for x in references])
-    closest_len_refer = len(references[closest_idx])
-
-    if len_Noutput > closest_len_refer:
+    if len_trans > reference_length:
         bp = 1
     else:
-        bp = np.exp(1 - (closest_len_refer / len(sentence)))
+        bp = np.exp(1 - float(reference_length) / len_trans)
 
-    BLEU_score = bp * np.exp(np.log(clipped_count / len_Noutput))
+    BLEU_score = bp * precision
 
     return BLEU_score
