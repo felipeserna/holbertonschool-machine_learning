d40ef70f3b0bd8807bb1082534d316e4b12714ee
felipeserna <feserna86@gmail.com>
2021-05-19 20:34:49 -0500

    RNN Encoder

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -6,7 +6,7 @@ to encode for machine translation
 import tensorflow as tf
 
 
-class RNNEncoder:
+class RNNEncoder(tf.keras.layers.Layer):
     """
     Inherits from `tensorflow.keras.layers.Layer`
     to encode for machine translation
@@ -15,15 +15,17 @@ class RNNEncoder:
         """
         Class constructor
         """
+        super(RNNEncoder, self).__init__()
         # Batch size
         self.batch = batch
         # Number of hidden units in the RNN cell
         self.units = units
         # Embedding layer that converts
         # words from the vocabulary into an embedding vector.
-        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
         # GRU layer
-        self.gru = tf.keras.layers.GRU(units,
+        self.gru = tf.keras.layers.GRU(units=units,
                                        return_sequences=True,
                                        return_state=True,
                                        recurrent_initializer='glorot_uniform')
@@ -33,11 +35,15 @@ class RNNEncoder:
         Returns: a tensor of shape (batch, units)
         containing the initialized hidden states
         """
-        init_hidden = tf.zeros([self.batch, self.units])
-        return init_hidden
+        init_hidden = tf.keras.initializers.Zeros()
+
+        return init_hidden(shape=(self.batch, self.units))
 
     def call(self, x, initial):
         """
         Returns: outputs, hidden
         """
-        return None
+        embeddings = self.embedding(x)
+        outputs, hidden = self.gru(embeddings, initial_state=initial)
+
+        return outputs, hidden
