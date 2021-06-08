c4719586218f175d1be0db3ed35d730b2ffce0f0
felipeserna <feserna86@gmail.com>
2021-06-08 12:23:32 -0500

    working and passing pycodestyle

--- supervised_learning/0x13-qa_bot/4-qa.py
+++ supervised_learning/0x13-qa_bot/4-qa.py
@@ -2,13 +2,8 @@
 """
 Answers questions from multiple reference texts
 """
-import tensorflow as tf
-import tensorflow_hub as hub
-from transformers import BertTokenizer
-import os
-import numpy as np
-question_answer_2 = __import__('2-qa').question_answer
-semantic_search = __import__('3-semantic_search').semantic_search
+qa_2 = __import__('2-qa').question_answer
+semantic_s = __import__('3-semantic_search').semantic_search
 
 
 def question_answer(corpus_path):
@@ -22,4 +17,4 @@ def question_answer(corpus_path):
             print("A: Goodbye")
             exit()
         else:
-            print("A:", question_answer_2(question, semantic_search(corpus_path, question)))
+            print("A:", qa_2(question, semantic_s(corpus_path, question)))
