8ee2b35566949e35e7289afd354af0b7dbc84584
felipeserna <feserna86@gmail.com>
2021-06-08 13:28:09 -0500

    'do not understand' was added

--- supervised_learning/0x13-qa_bot/4-qa.py
+++ supervised_learning/0x13-qa_bot/4-qa.py
@@ -2,7 +2,7 @@
 """
 Answers questions from multiple reference texts
 """
-qa_2 = __import__('2-qa').question_answer
+qa_0 = __import__('0-qa').question_answer
 semantic_s = __import__('3-semantic_search').semantic_search
 
 
@@ -17,4 +17,9 @@ def question_answer(corpus_path):
             print("A: Goodbye")
             exit()
         else:
-            print("A:", qa_2(question, semantic_s(corpus_path, question)))
+            text = semantic_s(corpus_path, question)
+            answer = qa_0(question, text)
+            if answer is None:
+                print("A: Sorry, I do not understand your question.")
+            else:
+                print("A:", answer)
