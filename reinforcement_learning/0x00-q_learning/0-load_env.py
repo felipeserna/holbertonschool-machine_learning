79e69cfb7e79b024a6c2917d48413cdcd860c335
felipeserna <feserna86@gmail.com>
2021-06-21 14:37:10 -0500

    skeleton

---
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Loads the pre-made FrozenLakeEnv environment
+from OpenAI’s gym
+"""
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Returns: the environment
+    """
+    
