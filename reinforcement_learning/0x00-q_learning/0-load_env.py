609bdbefab243d855969bbcb6306e1a450151752
felipeserna <feserna86@gmail.com>
2021-06-22 11:26:06 -0500

    Load the environment

--- reinforcement_learning/0x00-q_learning/0-load_env.py
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -3,10 +3,19 @@
 Loads the pre-made FrozenLakeEnv environment
 from OpenAI’s gym
 """
+import numpy as np
+import gym
+import random
+import time
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """
     Returns: the environment
     """
-    
+    # Creating The Environment
+    env = gym.make("FrozenLake-v0", desc=desc,
+                   map_name=map_name,
+                   is_slippery=is_slippery)
+
+    return env
