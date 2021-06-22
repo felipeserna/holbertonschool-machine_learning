5cf9f58295058063b2dbfb1f0ccb730e6386581c
felipeserna <feserna86@gmail.com>
2021-06-22 18:10:17 -0500

    Initialize Q-table

--- reinforcement_learning/0x00-q_learning/1-q_init.py
+++ reinforcement_learning/0x00-q_learning/1-q_init.py
@@ -2,10 +2,17 @@
 """
 Initializes the Q-table
 """
+import numpy as np
 
 
 def q_init(env):
     """
     Returns: the Q-table as a numpy.ndarray of zeros
     """
-    
+    # Creating the Q-Table
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.n
+
+    q_table = np.zeros((state_space_size, action_space_size))
+
+    return q_table
