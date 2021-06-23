404cf998c080fe289ed2d3d900ea42c3ce973fbf
felipeserna <feserna86@gmail.com>
2021-06-22 21:30:59 -0500

    first attempt

--- reinforcement_learning/0x00-q_learning/4-play.py
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -7,6 +7,33 @@ import numpy as np
 
 def play(env, Q, max_steps=100):
     """
-    Returns: the total rewards for the episode
+    You should always exploit the Q-table.
+    Returns: the total rewards for the episode.
     """
-    
+    env.reset()
+
+    for episode in range(1):
+        state = env.reset()
+        step = 0
+        done = False
+        print("****************************************************")
+        print("EPISODE ", episode)
+
+        for step in range(max_steps):
+        
+            # Take the action (index) that have the maximum expected future reward given that state
+            action = np.argmax(Q[state,:])
+        
+            new_state, reward, done, info = env.step(action)
+        
+            if done:
+                # Here, we decide to only print the last state (to see if our agent is on the goal or fall into an hole)
+                env.render()
+            
+                # We print the number of step it took.
+                print("Number of steps", step)
+                break
+            state = new_state
+    env.close()
+
+    return reward
