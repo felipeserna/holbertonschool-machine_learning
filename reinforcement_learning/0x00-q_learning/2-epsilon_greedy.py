6364b8638447a9a69047c0e2e5fc6ffac278c73a
felipeserna <feserna86@gmail.com>
2021-06-22 19:14:37 -0500

    Epsilon Greedy

--- reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/0x00-q_learning/2-epsilon_greedy.py
@@ -9,4 +9,17 @@ def epsilon_greedy(Q, state, epsilon):
     """
     Returns: the next action index
     """
-    
+    p = np.random.uniform(0, 1)
+
+    if p < epsilon:
+        """
+        Explore: select a random action
+        """
+        action = np.random.randint(Q.shape[1])
+    else:
+        """
+        Exploit: select the action with max value (future reward)
+        """
+        action = np.argmax(Q[state, :])
+
+    return action
