471edb84284a4cba077a141ff8e3ac3e75646db1
felipeserna <feserna86@gmail.com>
2021-06-22 21:40:31 -0500

    second attempt

--- reinforcement_learning/0x00-q_learning/4-play.py
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -11,29 +11,25 @@ def play(env, Q, max_steps=100):
     Returns: the total rewards for the episode.
     """
     env.reset()
+    state = env.reset()
+    done = False
+    for step in range(max_steps):
 
-    for episode in range(1):
-        state = env.reset()
-        step = 0
-        done = False
-        print("****************************************************")
-        print("EPISODE ", episode)
+        # Take the action (index) that have the maximum expected future reward
+        # given that state
+        action = np.argmax(Q[state, :])
 
-        for step in range(max_steps):
-        
-            # Take the action (index) that have the maximum expected future reward given that state
-            action = np.argmax(Q[state,:])
-        
-            new_state, reward, done, info = env.step(action)
-        
-            if done:
-                # Here, we decide to only print the last state (to see if our agent is on the goal or fall into an hole)
-                env.render()
-            
-                # We print the number of step it took.
-                print("Number of steps", step)
-                break
-            state = new_state
+        new_state, reward, done, info = env.step(action)
+
+        if done:
+            # Here, we decide to only print the last state
+            # (to see if our agent is on the goal or fall into an hole)
+            env.render()
+
+            # We print the number of step it took.
+            print("Number of steps", step)
+            break
+        state = new_state
     env.close()
 
     return reward
