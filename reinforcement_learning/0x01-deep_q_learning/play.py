75135b18f6647a2620592d634152f95524a71730
felipeserna <feserna86@gmail.com>
2021-06-26 21:07:43 -0500

    skeleton

---
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Script that can display a game played by the agent trained by train.py:
+* Load the policy network saved in policy.h5
+* Your agent should use the GreedyQPolicy
+"""
