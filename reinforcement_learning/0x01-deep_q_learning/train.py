7ac5c15d99d81625333cff1a998741385d923604
felipeserna <feserna86@gmail.com>
2021-06-26 21:03:20 -0500

    skeleton

---
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Script that utilizes keras, keras-rl, and gym
+to train an agent that can play Atari’s Breakout:
+* Use keras-rl‘s DQNAgent, SequentialMemory, and EpsGreedyQPolicy
+* Save the final policy network as policy.h5
+"""
