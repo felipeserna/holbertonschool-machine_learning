25d37d7384ea8d4b92ba5071dea7d78104f899b6
felipeserna <feserna86@gmail.com>
2021-07-11 12:37:35 -0500

    TD lambtha for Frozen Lake

--- reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -2,7 +2,6 @@
 """
 Performs the TD(λ) algorithm
 """
-import gym
 import numpy as np
 
 
@@ -11,3 +10,29 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100,
     """
     Returns: V, the updated value estimate
     """
+    n = env.observation_space.n
+
+    # Eligibility traces
+    Et = [0 for _ in range(n)]
+
+    for _ in range(episodes):
+        state = env.reset()
+        for _ in range(max_steps):
+            Et = list(np.array(Et) * lambtha * gamma)
+            Et[state] += 1.0
+            action = policy(state)
+            new_state, reward, done, _ = env.step(action)
+            # Goal
+            if env.desc.reshape(n)[new_state] == b'G':
+                reward = 1
+            # Hole
+            if env.desc.reshape(n)[new_state] == b'H':
+                reward = -1
+            # TD error
+            delta_t = reward + gamma * V[new_state] - V[state]
+            # V(s) <-- V(s) + alpha * delta_t * Et(s)
+            V[state] = V[state] + alpha * delta_t * Et[state]
+            if done:
+                break
+            state = new_state
+    return V
