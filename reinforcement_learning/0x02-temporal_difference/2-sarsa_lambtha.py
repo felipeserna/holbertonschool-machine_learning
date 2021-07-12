95c29cba2b710caae003b628a3411142748fe2e7
felipeserna <feserna86@gmail.com>
2021-07-11 22:52:46 -0500

    SARSA lambda in Frozen Lake

--- reinforcement_learning/0x02-temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/0x02-temporal_difference/2-sarsa_lambtha.py
@@ -2,7 +2,6 @@
 """
 Performs SARSA(λ)
 """
-import gym
 import numpy as np
 
 
@@ -12,3 +11,56 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100,
     """
     Returns: Q, the updated Q table
     """
+    state_space_size = env.observation_space.n
+    eps = epsilon
+
+    # Eligibility traces
+    Et = np.zeros((Q.shape))
+
+    for i in range(episodes):
+        state = env.reset()
+        action = epsilon_greedy(Q, state, epsilon)
+        for _ in range(max_steps):
+            Et *= lambtha * gamma
+            Et[state, action] += 1
+            new_state, reward, done, _ = env.step(action)
+            new_action = epsilon_greedy(Q, new_state, epsilon)
+            # Goal
+            if env.desc.reshape(state_space_size)[new_state] == b'G':
+                reward = 1
+            # Hole
+            if env.desc.reshape(state_space_size)[new_state] == b'H':
+                reward = -1
+            # Action-value form of the TD error
+            delta_t = \
+                reward + gamma * Q[new_state, new_action] - Q[state, action]
+            # It has the same update rule as TD(lambtha)
+            Q[state, action] = \
+                Q[state, action] + alpha * delta_t * Et[state, action]
+            if done:
+                break
+            state = new_state
+            action = new_action
+        epsilon = \
+            min_epsilon + (eps - min_epsilon) * np.exp(-epsilon_decay * i)
+    return Q
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """
+    Returns: the next action index
+    """
+    p = np.random.uniform(0, 1)
+
+    if p < epsilon:
+        """
+        Explore: select a random action
+        """
+        action = np.random.randint(Q.shape[1])
+    else:
+        """
+        Exploit: select the action with max value (future reward)
+        """
+        action = np.argmax(Q[state, :])
+
+    return action
