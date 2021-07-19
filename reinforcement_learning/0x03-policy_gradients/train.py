ae076dc3c78857ab9c6227ac88bb634976fc49fe
Felipe Serna <feserna86@gmail.com>
2021-07-18 23:13:47 -0500

    training rendering the environment

--- reinforcement_learning/0x03-policy_gradients/train.py
+++ reinforcement_learning/0x03-policy_gradients/train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-Implement the training
-(by using the Monte-Carlo policy gradient algorithm - also called REINFORCE).
+Implements a full training.
+Renders the environment every 1000 episodes computed.
 """
 
 
@@ -9,40 +9,41 @@ from policy_gradient import policy_gradient
 import numpy as np
 
 
-def train(env, nb_episodes, alpha=0.00045, gamma=0.98, show_result=False):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     """
-    env: initial environment
-    nb_episodes: number of episodes used for training
-    alpha: the learning rate
-    gamma: the discount factor
-    show_result:  is True, render the environment every 1000 episodes computed.
+    Return: all values of the score
+    (sum of all rewards during one episode loop).
     """
-    weights = np.random.rand(4, 2)
-    episode = []
+    # 4 states and 2 actions
+    weight = np.random.rand(4, 2)
+    episode_rewards = []
 
-    for i in range(nb_episodes):
+    for episode in range(nb_episodes):
         state = env.reset()[None, :]
-        grads = []
+        gradients = []
         rewards = []
         score = 0
 
         while True:
-            if show_result and (i % 1000 == 0):
+            if show_result and (episode % 1000 == 0):
                 env.render()
-            action, grad = policy_gradient(state, weights)
+            action, grad = policy_gradient(state, weight)
             next_state, reward, done, _ = env.step(action)
             next_state = next_state[None, :]
-            grads.append(grad)
+            gradients.append(grad)
             rewards.append(reward)
             score += reward
             state = next_state
+
             if done:
                 break
 
-        for j in range(len(grads)):
-            weights += alpha * grads[j] *\
-                sum([r * gamma ** r for t, r in enumerate(rewards[j:])])
-        episode.append(score)
-        print("{}: {}".format(i, score), end="\r", flush=False)
+        for i in range(len(gradients)):
+            weight += alpha * gradients[i] *\
+                sum([r * gamma ** r for _, r in enumerate(rewards[i:])])
 
-    return (episode)
+        episode_rewards.append(score)
+
+        print("{}: {}".format(episode, score), end="\r", flush=False)
+
+    return episode_rewards
