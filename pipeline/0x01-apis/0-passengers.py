9577011dd40efa010fa34c6098c3a0de38db4d64
Felipe Serna <feserna86@gmail.com>
2021-07-24 17:08:40 -0500

    skeleton and main file

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Swapi API. The Star Wars API
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Return: list of ships that can hold a given number of passengers
+    """
+    return None
