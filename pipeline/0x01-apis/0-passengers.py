f6de9b5d5275841d6e344c94bdde43794afdad6b
Felipe Serna <feserna86@gmail.com>
2021-07-25 19:08:49 -0500

    Can I join?

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -9,4 +9,21 @@ def availableShips(passengerCount):
     """
     Return: list of ships that can hold a given number of passengers
     """
-    return None
+    url = 'https://swapi-api.hbtn.io/api/starships/'
+
+    ships = []
+
+    while url is not None:
+        r = requests.get(url)
+        results = r.json()['results']
+
+        for ship in results:
+            p = ship["passengers"]
+            p = p.replace(',', '')
+
+            if p.isnumeric() and int(p) >= passengerCount:
+                ships.append(ship["name"])
+
+        url = r.json()["next"]
+
+    return ships
