8355d6bbcca657a45c5537c784fcc4282db35d02
Felipe Serna <feserna86@gmail.com>
2021-07-25 22:22:12 -0500

    Where I am?

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -25,6 +25,12 @@ def sentientPlanets():
                     planet = requests.get(planet_url).json()["name"]
                     planets.append(planet)
 
+        """
+        Pagination helps to limit the number of results
+        to help keep network traffic in check.
+        """
+        # There are a lot of species in different pages
+        # e.g., "next": "https://swapi-api.hbtn.io/api/species/?page=2"
         url = requests.get(url).json()["next"]
 
     return planets
