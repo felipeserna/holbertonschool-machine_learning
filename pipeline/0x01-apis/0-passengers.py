036f4f48be68265e68b96dc7ae01dd771c123640
Felipe Serna <feserna86@gmail.com>
2021-07-25 22:15:04 -0500

    pagination explanation

--- pipeline/0x01-apis/0-passengers.py
+++ pipeline/0x01-apis/0-passengers.py
@@ -24,6 +24,12 @@ def availableShips(passengerCount):
             if passengers.isnumeric() and int(passengers) >= passengerCount:
                 ships.append(ship["name"])
 
+        """
+        Pagination helps to limit the number of results
+        to help keep network traffic in check.
+        """
+        # There are a lot of starships in different pages
+        # e.g., "next": "https://swapi-api.hbtn.io/api/starships/?page=2"
         url = requests.get(url).json()["next"]
 
     return ships
