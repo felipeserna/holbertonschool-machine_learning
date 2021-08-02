b85c2a31fac3146c68edc5bca6f8af1e958f5a0c
Felipe Serna <feserna86@gmail.com>
2021-08-02 12:36:47 -0500

    Average score

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,15 @@
+-- Creates a stored procedure ComputeAverageScoreForUser that computes and store
+-- the average score for a student.
+delimiter //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id_new INT
+    )
+       BEGIN
+           UPDATE users SET average_score=(
+            SELECT AVG(score) FROM corrections
+            WHERE user_id = user_id_new)
+         WHERE id=user_id_new;
+       END//
+
+delimiter ;
