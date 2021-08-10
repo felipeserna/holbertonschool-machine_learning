ea5183aa50ef42b055ed57fb312b381e94669cac
Felipe Serna <feserna86@gmail.com>
2021-08-09 21:31:56 -0500

    shear

--- pipeline/0x04-data_augmentation/3-shear.py
+++ pipeline/0x04-data_augmentation/3-shear.py
@@ -9,4 +9,19 @@ def shear_image(image, intensity):
     """
     Returns the sheared image
     """
-    
+    # Converts a PIL Image instance to a Numpy array.
+    numpy_array = tf.keras.preprocessing.image.img_to_array(
+        image
+    )
+
+    # Performs a random spatial shear of a Numpy image tensor.
+    sheared_numpy = tf.keras.preprocessing.image.random_shear(
+        numpy_array, intensity, row_axis=0, col_axis=1, channel_axis=2
+    )
+
+    # Converts a 3D Numpy array to a PIL Image instance.
+    pil_image = tf.keras.preprocessing.image.array_to_img(
+        sheared_numpy
+    )
+
+    return pil_image
